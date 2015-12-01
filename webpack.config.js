e145be5a4c97dc84e9484c97b1959beecf16177e
Jeff Barczewski <jeff.barczewski@gmail.com>
2015-12-01 15:30:38 -0600

    combined browserify and webpack operations

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/joi-browser.js',
+  output: {
+    libary: 'Joi',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    path: __dirname + '/dist',
+    filename: 'webpack-bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // need to babelify joi, isemail, hoek, and topo's lib
+        test: /\/node_modules\/(joi\/lib\/|isemail\/lib\/|hoek\/lib\/|topo\/lib\/)/,
+        loader: 'babel'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  node: {
+    crypto: 'empty',
+    net: 'empty',
+    dns: 'empty'
+  },
+  plugins: [
+    // english locale is included, exclude the rest
+    new webpack.IgnorePlugin(/locale/, /moment$/)
+  ]
+};
